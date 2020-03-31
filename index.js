57633420e16bfde809e0e947f15c520377005151
msobalvarro <mssrsobalvarro1997@gmail.com>
2020-03-30 18:39:33 -0600

    testing for production

--- index.js
+++ index.js
@@ -34,11 +34,24 @@ const UpgradePlan = require('./controller/upgradePlan')
 app.use(useragent.express())
 
 // Use configuration in developer MODE
-app.use(cors({
-	allowedHeaders: {
-		"Access-Control-Allow-Origin": "https://backoffice-speedtradings.herokuapp.com"
-	}
-}))
+app.use((req, res, next) => {
+
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', '*')
+
+	// Request methods you wish to allow
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+
+	// Request headers you wish to allow
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader('Access-Control-Allow-Credentials', true)
+
+	// Pass to next layer of middleware
+	next()
+})
 
 // User for parse get json petition
 app.use(bodyParse.json())
