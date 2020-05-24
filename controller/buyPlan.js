daa30d7112c79e52be3b0916d40e3735ce0c7f79
msobalvarro <mssrsobalvarro1997@gmail.com>
2020-05-24 15:30:57 -0600

    hash validator integrated into in buy plan

--- controller/buyPlan.js
+++ controller/buyPlan.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { check, validationResult } = require('express-validator')
+const { bitcoin, ethereum } = require("../middleware/hash")
 const WriteError = require('../logs/write')
 
 // MiddleWare
@@ -8,7 +9,7 @@ const Auth = require('../middleware/auth')
 
 // Mysql
 const query = require('../config/query')
-const { createPlan } = require('./queries')
+const { createPlan, searchHash } = require('./queries')
 
 router.get('/', (_, res) => res.status(500))
 
@@ -20,7 +21,7 @@ router.post('/', [
         check('hash', 'Hash is required').exists(),
         check('amount', 'Amount is required or Invalid').isNumeric(),
     ]
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req)
 
     if (!errors.isEmpty()) {
@@ -35,17 +36,42 @@ router.post('/', [
     try {
         const { id_currency, id_user, hash, amount } = req.body
 
-        query(createPlan, [id_currency, id_user, hash, amount], async (response) => {
-            if (clients) {
-                clients.forEach(async (client) => {
-                    await client.send("newRequest")
+        // Buscamos que el hash exista para avisar al usuario
+        query(searchHash, [hash], response => {
+            if (response[0].length > 0) {
+                res.send({
+                    error: true,
+                    message: "El hash ya esta registrado"
                 })
             }
-
-            res.send(response[0][0])
-        }).catch(reason => {
-            throw reason
         })
+
+        const comprobate = id_currency === 1 ? bitcoin : ethereum
+
+        // Comprobamos el hash
+        const responseHash = await comprobate(hash, amount)
+
+        // Verificamos si hay un error 
+        if (responseHash.error) {
+            res.send({
+                error: true,
+                message: responseHash.message
+            })
+        } else {
+            // Creamos la solicitud
+            query(createPlan, [id_currency, id_user, hash, amount], async (response) => {
+                if (clients) {
+                    clients.forEach(async (client) => {
+                        await client.send("newRequest")
+                    })
+                }
+
+                res.send(response[0][0])
+            }).catch(reason => {
+                throw reason
+            })
+        }
+
     } catch (error) {
         WriteError(`buyPlan.js - catch execute query | ${error}`)
 
