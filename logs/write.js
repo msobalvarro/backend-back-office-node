fbe280496364bc276d6a3e27a25f69b260f9d075
msobalvarro <mssrsobalvarro1997@gmail.com>
2020-05-28 10:20:41 -0600

    delete logs for app

--- logs/write.js
+++ logs/write.js
@@ -1,5 +1,4 @@
 const log = require('simple-node-logger').createSimpleLogger('./logs/logs.log')
-const logApp = require('simple-node-logger').createSimpleLogger('./logs/app.log')
 
 /**Register new message to log archive */
 module.exports = (errStr = '', type = 'error') => {
