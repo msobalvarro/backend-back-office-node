6af71e89064c5d395a9b879140afab9dd3fc0375
msobalvarro <mssrsobalvarro1997@gmail.com>
2020-05-28 10:16:08 -0600

    check trading api

--- controller/admin/trading.js
+++ controller/admin/trading.js
@@ -14,10 +14,12 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const query = require("../../config/query")
 const { getDataTrading } = require("../queries")
 
-router.post('/', [
+const checkParamsRequest = [
     check('id_currency', 'ID currency is required').isInt(),
     check('percentage', 'Percentaje is requerid').isFloat()
-], async (req, res) => {
+]
+
+router.post('/', checkParamsRequest, async (req, res) => {
     try {
         const errors = validationResult(req)
 
@@ -34,61 +36,61 @@ router.post('/', [
 
         const queryScript = (_id = 0, _percentage = 0, _amount = 0) => `call createPayment(${_id}, ${_percentage}, ${_amount});`
 
-        query(getDataTrading, [id_currency], async (response) => {
-            for (let index = 0; index < response[0].length; index++) {
+        query.withPromises(getDataTrading, [id_currency])
+            .then(async (response) => {
+                for (let index = 0; index < response[0].length; index++) {
 
-                const { amount, email, name, id } = response[0][index]
+                    const { amount, email, name, id } = response[0][index]
 
-                // Creamos el nuevo monto a depositar
-                // `percentage (0.5 - 1)%`
-                const newAmount = (percentage * amount) / 100
+                    // Creamos el nuevo monto a depositar
+                    // `percentage (0.5 - 1)%`
+                    const newAmount = (percentage * amount) / 100
 
-                const msg = {
-                    to: email,
-                    from: 'dashboard@speedtradings.com',
-                    subject: `Informe de ganancias ${moment().format('"DD-MM-YYYY"')}`,
-                    html: `
-                    <div
-                        style="background: #161616; padding: 25px; color: #000; font-size: 1.2em; font-family: Arial, Helvetica, sans-serif; text-align: center; height: 100%;">
-                        <a href="https://www.speedtradings.com/">
-                            <img width="512" height="256"
-                                src="https://lh3.googleusercontent.com/XtcodcoWRFK2wQXbDEv6q6RJ26lHZHuSBEn3yBkpzh2dmuWZc546Mm128xdoTjtFIEWUVFp2DjFSB4Bfz44wSfD17QqpogYvq8UBRHtLWb9DZuD9qziilG_J8pEOwigJKfM85zLmWZKR825axHJuR49JD_Q499xq7bgc_2-UjiQI97OdFh-pGgN8jbhmepRHhUmazh_WC3BuZBcw70VSpJGDOBd8Qbqtl0jyDWcT-yUTl3chpl45DmHEwhB0F3updv61LRm96Vz9GRD1EM3ftmzKbAET_M3SON_5QNinYlMH20oqJsmvQ-wBlXiLoDssrlKu-QgvfVaYdQD4l4_9pnqOUzqeRzpIwEMbPMq21MS96ySQVottkdT2aV5ViqOXKvCGhgi0rcBMgEhdhOO7N7X467ohDH26hLgL7gv9XV-VhClkv4X5zn1ykbda2Mpx7ZrwG3LroS3Qxb1xt7J3YyS5uA_7VXoUIlmRW1tijC-itvyVyS5BK4skiazCJIedvWbEFEes1IoGw3BW0YHv6LjC-peUV7CiB2Fib4b79qwxrIdGYOv4UN9dHYHCV4QHaEFe4wb8NMRpTrbO8Et-5vn7mxL2aQn7IziZr-3hra2E5CboMxYrYhMTiXAnEmMTFr3Q4G3ywafX96q4qBIQlF8PHhs6cDciS4NHMKu1CMqOX9c3n66WlLapannN0j02aYF-NA=w1600-h828-ft" />
-                        </a>
-
-                        <br />
-
-                        <h1 style="color: #FFF;">Estimado/a ${name}</h1>
-
-                        <p style="color: #FFF;">
-                            Le informamos que las ganancias generadas por el trading del día de hoy ya fueron aplicadas.
-                        </p>
-
+                    const msg = {
+                        to: email,
+                        from: 'dashboard@speedtradings.com',
+                        subject: `Informe de ganancias ${moment().format('"DD-MM-YYYY"')}`,
+                        html: `
                         <div
-                            style="padding: 25px; background-color: #33778833; color: #FFF; margin-top: 10px; border-radius: 10px; text-transform: uppercase;">
-                            <p>
-                                Corresponde al <b>${percentage}%</b> que equivale a <b>${newAmount} ${typeCoin}</b>
+                            style="background: #161616; padding: 25px; color: #000; font-size: 1.2em; font-family: Arial, Helvetica, sans-serif; text-align: center; height: 100%;">
+                            <a href="https://www.speedtradings.com/">
+                                <img width="512" height="256"
+                                    src="https://lh3.googleusercontent.com/XtcodcoWRFK2wQXbDEv6q6RJ26lHZHuSBEn3yBkpzh2dmuWZc546Mm128xdoTjtFIEWUVFp2DjFSB4Bfz44wSfD17QqpogYvq8UBRHtLWb9DZuD9qziilG_J8pEOwigJKfM85zLmWZKR825axHJuR49JD_Q499xq7bgc_2-UjiQI97OdFh-pGgN8jbhmepRHhUmazh_WC3BuZBcw70VSpJGDOBd8Qbqtl0jyDWcT-yUTl3chpl45DmHEwhB0F3updv61LRm96Vz9GRD1EM3ftmzKbAET_M3SON_5QNinYlMH20oqJsmvQ-wBlXiLoDssrlKu-QgvfVaYdQD4l4_9pnqOUzqeRzpIwEMbPMq21MS96ySQVottkdT2aV5ViqOXKvCGhgi0rcBMgEhdhOO7N7X467ohDH26hLgL7gv9XV-VhClkv4X5zn1ykbda2Mpx7ZrwG3LroS3Qxb1xt7J3YyS5uA_7VXoUIlmRW1tijC-itvyVyS5BK4skiazCJIedvWbEFEes1IoGw3BW0YHv6LjC-peUV7CiB2Fib4b79qwxrIdGYOv4UN9dHYHCV4QHaEFe4wb8NMRpTrbO8Et-5vn7mxL2aQn7IziZr-3hra2E5CboMxYrYhMTiXAnEmMTFr3Q4G3ywafX96q4qBIQlF8PHhs6cDciS4NHMKu1CMqOX9c3n66WlLapannN0j02aYF-NA=w1600-h828-ft" />
+                            </a>
+
+                            <br />
+
+                            <h1 style="color: #FFF;">Estimado/a ${name}</h1>
+
+                            <p style="color: #FFF;">
+                                Le informamos que las ganancias generadas por el trading del día de hoy ya fueron aplicadas.
                             </p>
+
+                            <div
+                                style="padding: 25px; background-color: #33778833; color: #FFF; margin-top: 10px; border-radius: 10px; text-transform: uppercase;">
+                                <p>
+                                    Corresponde al <b>${percentage}%</b> que equivale a <b>${newAmount} ${typeCoin}</b>
+                                </p>
+                            </div>
+
+                            <p style="color: #FFF;">
+                                Puede confirmarlo en su Dashboard. Gracias por confiar en nosotros.
+                            </p>
+
+                            <br />
+
+                            <b style="color: #20a5c2; font-size: 14px;">Saludos, Equipo de Speed Tradings Bank.</b>
                         </div>
-
-                        <p style="color: #FFF;">
-                            Puede confirmarlo en su Dashboard. Gracias por confiar en nosotros.
-                        </p>
-
-                        <br />
-
-                        <b style="color: #20a5c2; font-size: 14px;">Saludos, Equipo de Speed Tradings Bank.</b>
-                    </div>
                     `,
+                    }
+
+                    await sgMail.send(msg)
+
+                    await query(queryScript(id, percentage, newAmount), [], () => { }).catch(reason => { throw reason })
                 }
 
-                await sgMail.send(msg)
-
-                await query(queryScript(id, percentage, newAmount), [], (response) => { }).catch(reason => { throw reason })
-            }
-
-            res.status(200).send({ response: 'success' })
-
-        }).catch(reason => { throw reason })
+                res.status(200).send({ response: 'success' })
+            })
     } catch (error) {
         WriteError(`trading.js - catch execute query | ${error}`)
 
