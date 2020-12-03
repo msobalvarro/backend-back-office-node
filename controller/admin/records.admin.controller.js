419fddaa516e590a516da41b23eb3f1cd0b76799
Harold Espinoza <haroldesptru@gmail.com>
2020-12-03 15:35:51 -0600

    Ajuste en el endpoint de detalle del usuario, añadido endpoint para obtener la imagen del avatar que el usuario registro dentro de su informacion de kyc

--- controller/admin/records.admin.controller.js
+++ controller/admin/records.admin.controller.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const { check, validationResult } = require('express-validator')
+
 const WriteError = require('../../logs/write.config')
+const { downloadFile } = require('../../configuration/constant.config')
 
 // Sql transaction
 const sql = require("../../configuration/sql.config")
-const { getAllRecords, getRecordDetails } = require("../../configuration/queries.sql")
+const {
+    getAllRecords,
+    getRecordDetails,
+    getUserAvatarPicture
+} = require("../../configuration/queries.sql")
 
 router.get('/', async (_, res) => {
     try {
@@ -25,15 +30,9 @@ router.get('/', async (_, res) => {
     }
 })
 
-router.post('/id', [check('id', 'ID is not valid').isInt()], async (req, res) => {
+router.get('/:id', async (req, res) => {
     try {
-        const errors = validationResult(req)
-
-        if (!errors.isEmpty()) {
-            throw String(errors.array()[0].msg)
-        }
-
-        const { id } = req.body
+        const { id } = req.params
 
         const response = await sql.run(getRecordDetails, [id])
 
@@ -44,6 +43,48 @@ router.post('/id', [check('id', 'ID is not valid').isInt()], async (req, res) =>
         WriteError(`records.js - catch execute sql | ${error}`)
 
         res.send({ error: true, message: error })
+    }
+})
+
+router.get('/:id/avatar', async (req, res) => {
+    try {
+        const { id } = req.params
+
+        // Obtiene ls información del avatar del usuario
+        const result = await sql.run(getUserAvatarPicture, [id])
+
+        if (result.length === 0) {
+            throw String('Avatar picture not exist')
+        }
+
+        // Se extrae el nombre y el tipo de archivo
+        const { filename, type } = result[0]
+
+        // Se obtiene la imagen desde el bucket
+        const {
+            result: downloadResult,
+            data,
+            error: downloadError
+        } = await downloadFile(filename)
+
+        if (!downloadResult) {
+            throw downloadError
+        }
+
+        res.set({
+            'Cache-Control': 'no-cache',
+            'Content-Type': type,
+            'Content-Length': data.length
+        })
+
+        res.send(data[0])
+    } catch (message) {
+        WriteError(`records.admin.controller.js | Error to get user avatar | ${message.toString()}`)
+
+        res.send({
+            error: true,
+            message
+        })
     }
 })
 
